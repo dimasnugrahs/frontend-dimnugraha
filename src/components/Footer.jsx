c10d47db4d950cd45d8529187ff4618b9bc5803b
dimasnugrahs <79310230+dimasnugrahs@users.noreply.github.com>
2025-10-29 11:13:45 +0800

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,13 @@ function Footer() {
   return (
     <footer>
       <div className="bg-company-600 text-company-50 py-20 text-center">
-        <div>Hello world</div>
+        <div>
+          Just live like playing a game, running side quest and main quest,
+          grinding to the next leveling up.
+        </div>
+        <p>Icon for Social Media</p>
+        <p>Build with Love (C) DimNugraha</p>
+        <p>this website is built with NextJS</p>
       </div>
     </footer>
   );
